1686a3df22e0fb6aafd3ec526d19e9a7035a1d9e
Mathieu Le Ray <leraymathieu@gmail.com>
2023-12-26 15:17:51 +0100

    first commit

---
+++ app/src/index.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+$a = 'Hello World </br>';
+$b = $a;
+
+//echo $b;
+$b = "Bonjour le monde";
+//echo $b;
+
+//echo gettype($a);
+
+function test($value)
+{
+    return 'Bonjour ' . $value . '</br>';
+}
+
+// echo match ($a) {
+//     //valeur => retour / exec une fonction,
+//     'Hello World' => test($a),
+//     'Hello World 2' => 'Coucou le monde',
+//     default => '</br>default'
+// };
+
+//opérateur de fusion
+$d = null;
+$b = 2;
+//echo $c = $d ?? $b ?? 'inconnu';
+
+
+function my_min(...$rest)
+{
+    static $count = 0; //garde dans la mémoire globale et non pas seulement dans le scope de la fonction
+    global $b; //2
+    print_r($rest);
+    $count++;
+}
+
+//my_min(1, 2, 3);
+
+
+//callback
+function func($callback)
+{
+    $callback(6);
+}
+
+func(function ($e) use ($b) {
+    //global $b;
+    echo $b + $e;
+});
+
+$greetWord = "Bonjour";
+
+$direBonjour = function ($name) use ($greetWord) {
+    echo "$greetWord $name <br>";
+};
+
+$direBonjour("Paul"); // Bonjour Paul
+$direBonjour("Jean"); // Bonjour Jean
+
+
+//typage de fonction
+$test = 42;
+$addition = fn (int | float $a, int $b): int | float => $a + $b + $test;
+
+echo $addition(2, 3); // 47
