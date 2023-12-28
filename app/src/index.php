841af5ee091f728d0ca1a3f57c544d0183a00ff9
Mathieu Le Ray <leraymathieu@gmail.com>
2023-12-28 12:57:07 +0100

    update index.php

--- app/src/index.php
+++ app/src/index.php
@@ -1,14 +1,61 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+class Reader
+{
+    function __construct(public $filename)
+    {
+        if (file_exists($filename)) {
+            $this->handle = fopen($filename, 'r');
+        } else {
+            echo "Le fichier n'existe pas";
+        }
+        echo 'construct call';
+    }
 
-<body>
+    function read()
+    {
+        echo fread($this->handle, 10);
+    }
 
-</body>
+    function __destruct()
+    {
+        fclose($this->handle);
+        echo 'destruct call';
+    }
+}
 
-</html>
+
+$reader = new Reader(__DIR__ . '/test.txt');
+
+$reader->read();
+
+//****************************************************************** */
+
+
+class User
+{
+    public bool $isAdmin = false;
+
+    function __construct(public string $prenom, public string $nom)
+    {
+    }
+
+    function direBonjour($prenom, $nom)
+    {
+        echo "Bonjour, je suis $prenom $nom";
+    }
+}
+
+class Admin extends User
+{
+    public bool $isAdmin = true;
+    function direBonjour($prenom, $nom)
+    {
+        parent::direBonjour($prenom, $nom);
+        echo " et je suis un admin. <br>";
+    }
+}
+
+$dupont = new Admin('Jean', 'Dupont');
+$dupont->direBonjour('Jean', 'Dupont'); // Bonjour, je suis Jean Dupont.
+echo $dupont->isAdmin; // 1
